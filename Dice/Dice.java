ccc2be7397ae65688b6dd7a6896da3711a60fac0
Patorn <PPMoss2537@gmail.com>
2016-06-05 02:37:54 -0400

    Added playAgain functionality

--- Dice/Dice.java
+++ Dice/Dice.java
@@ -7,13 +7,13 @@ public class Dice {
 	public void play() {
 		Random rand = new Random();
 		int diceRolled = 2 + rand.nextInt(12 - 2 + 1);
-		System.out.println("Flipping coins! " + diceRolled) ;
+		System.out.println("Rolling dice! " + diceRolled) ;
 	}
 	
 	public void playGame(){
 		int choice;
 		Scanner play = new Scanner(System.in);
-		
+		do {
 		System.out.println("Do you want to play dice?");
 		choice = play.nextInt();
 		if(choice == 1) { 
@@ -23,9 +23,11 @@ public class Dice {
 			System.out.println("Goodbye");
 			System.exit(0);
 		}
+	  }while(choice == 1);
 	}
 	
 	public static void main(String args[]){
-	
+		Dice roller = new Dice();
+		roller.playGame();
 	}
 }
