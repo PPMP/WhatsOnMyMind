7417b67c65a91abbe87be4ee4a376d5cda4b8206
Patorn <PPMoss2537@gmail.com>
2016-06-05 02:27:47 -0400

    Added play functionality

--- Dice/Dice.java
+++ Dice/Dice.java
@@ -1,15 +1,19 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Dice {
 	int number;
 	
 	public void play() {
-		
+		Random rand = new Random();
+		int diceRolled = 2 + rand.nextInt(12 - 2 + 1);
+		System.out.println("Flipping coins! " + diceRolled) ;
 	}
 	
 	public void playGame(){
 		int choice;
 		Scanner play = new Scanner(System.in);
+		
 		System.out.println("Do you want to play dice?");
 		choice = play.nextInt();
 		if(choice == 1) { 
@@ -22,6 +26,6 @@ public class Dice {
 	}
 	
 	public static void main(String args[]){
-
+	
 	}
 }
